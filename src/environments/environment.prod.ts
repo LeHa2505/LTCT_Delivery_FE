78f2ffada039e69b11453aa50de0ec355f63f4df
Dinh Thi Ngoc Anh <ngocanh732001@gmail.com>
2023-01-08 19:51:10 +0700

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   BASE_API_URI: {
-    BASE_SERVICE_SP10_API: 'http://tungsnk.tech:8082/',
+    BASE_SERVICE_SP10_API: 'https://hedspi.dev:8082/',
     MOCK_API:'https://6385e9a5beaa6458266ce8e3.mockapi.io/'
   },
 };
