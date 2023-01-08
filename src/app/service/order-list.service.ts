9e7a08b0ca4f9ff008089c6402a95bf58a3f43d5
Dinh Thi Ngoc Anh <ngocanh732001@gmail.com>
2023-01-08 20:21:21 +0700

    fix cors

--- src/app/service/order-list.service.ts
+++ src/app/service/order-list.service.ts
@@ -1,8 +1,13 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
+const options = {
+  headers: new HttpHeaders({
+    'Access-Control-Allow-Origin': '*'
+  }),
+};
 @Injectable({
   providedIn: 'root'
 })
@@ -11,16 +16,16 @@ export class OrderListService {
   constructor(private http: HttpClient) {}
 
   getShippingOrder(): Observable<any> {
-    return this.http.get(environment.BASE_API_URI.BASE_SERVICE_SP10_API + 'api/shipping_order');
+    return this.http.get(environment.BASE_API_URI.BASE_SERVICE_SP10_API + 'api/shipping_order',options);
   }
   getShippingOrderDetail(code : any): Observable<any> {
-    return this.http.get(environment.BASE_API_URI.BASE_SERVICE_SP10_API + 'api/shipping_order/' + code);
+    return this.http.get(environment.BASE_API_URI.BASE_SERVICE_SP10_API + 'api/shipping_order/' + code,options);
   }
   getProductList(code : any): Observable<any> {
-    return this.http.get(environment.BASE_API_URI.BASE_SERVICE_SP10_API + 'api/shipping_order/product/' + code);
+    return this.http.get(environment.BASE_API_URI.BASE_SERVICE_SP10_API + 'api/shipping_order/product/' + code,options);
   }
   updateStatus(orderCode : string, body : any) : Observable<any>{
-    return this.http.put(environment.BASE_API_URI.BASE_SERVICE_SP10_API + 'api/shipping_order/' + orderCode, body);
+    return this.http.put(environment.BASE_API_URI.BASE_SERVICE_SP10_API + 'api/shipping_order/' + orderCode, body,options);
   }
 
 }
