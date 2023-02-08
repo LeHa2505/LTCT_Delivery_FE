26588595d0a716de212ad66e4c11829e2530b941
LeHa2505 <leha25052001@gmail.com>
2023-02-09 02:49:33 +0700

    add pie chart

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { ChartOptions, ChartType, ChartDataset, Scale } from 'chart.js';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { DashboardService } from 'src/app/service/dashboard.service';
+import { BrowserModule } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -21,8 +23,7 @@ export class DashboardComponent implements OnInit {
   barChartOptions: ChartOptions = {
     responsive: true,
   };
-  barChartLabels: string[] = [
-  ];
+  barChartLabels: string[] = [];
   barChartType: ChartType = 'bar';
   barChartLegend = false;
   barChartPlugins = [];
@@ -38,23 +39,82 @@ export class DashboardComponent implements OnInit {
   ];
 
   // ---
-  data:any;
-  constructor(private ser : DashboardService, private mess : NzMessageService){}
+  data: any;
+  constructor(private ser: DashboardService, private mess: NzMessageService) {}
   ngOnInit(): void {
     this.ser.getDashboard().subscribe(
-      (res:any)=>{
+      (res: any) => {
         if (res.result.ok) {
           this.data = res.data;
-          this.data.orderQuantityPerDay.forEach((element:any) => {
+          this.data.orderQuantityPerDay.forEach((element: any) => {
             this.barChartLabels.push(element.createAt);
             this.barChartData[0].data.push(element.count);
           });
-        } else this.mess.error(res.result.message)
+        } else this.mess.error(res.result.message);
       },
-      (error)=>{
+      (error: any) => {
         this.mess.error('Có lỗi xảy ra!');
       }
-    )
+    );
   }
+  listOfData = [
+    {
+      orderCode: 'OD1133',
+      statusDetail: 'Giao thiếu hàng',
+      createAt: '2022-11-27',
+    },
+    {
+      orderCode: 'OD1133',
+      statusDetail: 'Giao thiếu hàng',
+      createAt: '2022-11-27',
+    },
+    {
+      orderCode: 'OD1133',
+      statusDetail: 'Giao thiếu hàng',
+      createAt: '2022-11-27',
+    },
+    {
+      orderCode: 'OD1133',
+      statusDetail: 'Giao thiếu hàng',
+      createAt: '2022-11-27',
+    },
+  ];
 
+  pieChartNumberOfSpendingOptions: ChartOptions = {
+    responsive: true,
+  };
+  pieChartNumberOfSpendingLabels: string[] = [
+    'Mon',
+    'Tue',
+    'Wed',
+    'Thu',
+    'Fri',
+    'Sat',
+    'Sun',
+  ];
+  pieChartNumberOfSpendingType: ChartType = 'pie';
+  pieChartNumberOfSpendingLegend = true;
+  pieChartNumberOfSpendingPlugins = [];
+  pieChartNumberOfSpendingData: ChartDataset[] = [
+    {
+      data: [45, 37, 60, 70, 46, 33, 40],
+      backgroundColor: [
+        '#93d9d9',
+        '#ffa1b5',
+        '#86c7f3',
+        '#ffe29a',
+        '#f1f2f4',
+        '#fa9092',
+        '#fed29d',
+      ],
+      borderColor: '#fff',
+      hoverBackgroundColor: '#597a9e',
+      hoverBorderWidth: 10,
+      pointBackgroundColor: '#007bff',
+      pointHoverBorderColor: '#007bff',
+      tension: 0.35,
+      fill: true,
+      label: '',
+    },
+  ];
 }
