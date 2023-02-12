3e4f0690ac63110dd896d25460a43dbe4a5a8ed5
LeHa2505 <leha25052001@gmail.com>
2023-02-12 18:52:38 +0700

    dashboard fix

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -4,7 +4,8 @@ import { ChartOptions, ChartType, ChartDataset, Scale } from 'chart.js';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { DashboardService } from 'src/app/service/dashboard.service';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { OrderListService } from 'src/app/service/order-list.service';
+import { E } from 'chart.js/dist/chunks/helpers.core';
 
 @Component({
   selector: 'app-dashboard',
@@ -40,64 +41,18 @@ export class DashboardComponent implements OnInit {
 
   // ---
   data: any;
-  constructor(private ser: DashboardService, private mess: NzMessageService) {}
-  ngOnInit(): void {
-    this.ser.getDashboard().subscribe(
-      (res: any) => {
-        if (res.result.ok) {
-          this.data = res.data;
-          this.data.orderQuantityPerDay.forEach((element: any) => {
-            this.barChartLabels.push(element.createAt);
-            this.barChartData[0].data.push(element.count);
-          });
-        } else this.mess.error(res.result.message);
-      },
-      (error: any) => {
-        this.mess.error('Có lỗi xảy ra!');
-      }
-    );
-  }
-  listOfData = [
-    {
-      orderCode: 'OD1133',
-      statusDetail: 'Giao thiếu hàng',
-      createAt: '2022-11-27',
-    },
-    {
-      orderCode: 'OD1133',
-      statusDetail: 'Giao thiếu hàng',
-      createAt: '2022-11-27',
-    },
-    {
-      orderCode: 'OD1133',
-      statusDetail: 'Giao thiếu hàng',
-      createAt: '2022-11-27',
-    },
-    {
-      orderCode: 'OD1133',
-      statusDetail: 'Giao thiếu hàng',
-      createAt: '2022-11-27',
-    },
-  ];
-
+  listOfData = [];
+  listOfDisplayData = [];
   pieChartNumberOfSpendingOptions: ChartOptions = {
     responsive: true,
   };
-  pieChartNumberOfSpendingLabels: string[] = [
-    'Mon',
-    'Tue',
-    'Wed',
-    'Thu',
-    'Fri',
-    'Sat',
-    'Sun',
-  ];
+  pieChartNumberOfSpendingLabels: string[] = [];
   pieChartNumberOfSpendingType: ChartType = 'pie';
   pieChartNumberOfSpendingLegend = true;
   pieChartNumberOfSpendingPlugins = [];
   pieChartNumberOfSpendingData: ChartDataset[] = [
     {
-      data: [45, 37, 60, 70, 46, 33, 40],
+      data: [],
       backgroundColor: [
         '#93d9d9',
         '#ffa1b5',
@@ -117,4 +72,64 @@ export class DashboardComponent implements OnInit {
       label: '',
     },
   ];
+  orderQuantityByStatus: number = 0;
+  constructor(
+    private ser: DashboardService,
+    private mess: NzMessageService,
+    private orderListService: OrderListService
+  ) {}
+  ngOnInit(): void {
+    this.ser.getDashboard().subscribe(
+      (res: any) => {
+        if (res.result.ok) {
+          this.data = res.data;
+          this.data.orderQuantityPerDay.forEach((element: any) => {
+            this.barChartLabels.push(element.createAt);
+            this.barChartData[0].data.push(element.count);
+          });
+          this.pieChartNumberOfSpendingData[0].data.push(
+            this.data.orderQuantityByStatus['3']
+          );
+          this.pieChartNumberOfSpendingData[0].data.push(
+            this.data.orderQuantityByStatus['4']
+          );
+          this.pieChartNumberOfSpendingData[0].data.push(
+            this.data.orderQuantityByStatus['7']
+          );
+          // console.log("jhghjgjhjhgjhg"+this.orderQuantityByStatus);
+          
+        } else this.mess.error(res.result.message);
+      },
+      (error: any) => {
+        this.mess.error('Có lỗi xảy ra!');
+      }
+    );
+
+    this.ser.getStatus().subscribe((res: any) => {
+      console.log(res);
+      res.forEach((element) => {
+        if (element.id == 3 || element.id == 4 || element.id == 7) {
+          this.pieChartNumberOfSpendingLabels.push(element.desc);
+        }
+      }, console.log(this.pieChartNumberOfSpendingLabels));
+    });
+
+    this.orderListService.getShippingOrder().subscribe(
+      (res: any) => {
+        console.log(res);
+        if (res.result.ok) {
+          this.listOfData = [...res.data];
+          this.listOfData.forEach((element) => {
+            if (element.statusCode.id == 5) {
+              this.listOfDisplayData.push(element);
+            }
+          }, console.log(this.listOfDisplayData));
+          // this.listOfDisplayData = [...this.listOfData]
+        } else this.mess.error(res.result.message);
+      },
+      (error) => {
+        this.mess.error('Có lỗi xảy ra!');
+      }
+    );
+  }
 }
